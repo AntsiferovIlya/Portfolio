ebc2fb5b2d6b8cbe4b3903e8d4d3738dcec7cd1e
AntsiferovIlya <113356009+AntsiferovIlya@users.noreply.github.com>
2022-09-19 17:57:20 +0300

    Add files via upload

--- Multithreading/Transactions/src/main/java/Bank.java
+++ Multithreading/Transactions/src/main/java/Bank.java
@@ -15,7 +15,8 @@ public class Bank {
     public synchronized boolean isFraud(String fromAccountNum, String toAccountNum, long amount)
             throws InterruptedException {
         Thread.sleep(1000);
-        return random.nextBoolean();
+//        return random.nextBoolean();
+        return false;
     }
 
     public void addAccountToBank(Account account) {
@@ -26,29 +27,43 @@ public class Bank {
         Account fromAccount = accounts.get(fromAccountNum);
         Account toAccount = accounts.get(toAccountNum);
         boolean chek = false;
-
-        if(isEnoughMoney(fromAccount.getMoney(), amount)) {
-            if(amount > MAX_AMOUNT) {
-                System.out.println("Сумма перевода боольше " + MAX_AMOUNT + "."
-                + "\n\tПодождите несколько секунд идет проверка счета.");
-                try {
-                    chek = isFraud(fromAccountNum, toAccountNum, amount);
-                } catch(InterruptedException e) {
-                    e.printStackTrace(System.out);
+        Account lowSyncAcc;
+        Account topSyncAcc;
+        if(fromAccount.hashCode() < toAccount.hashCode()) {
+            lowSyncAcc = fromAccount;
+            topSyncAcc = toAccount;
+        } else {
+            lowSyncAcc = toAccount;
+            topSyncAcc = fromAccount;
+        }
+        synchronized(lowSyncAcc) {
+            synchronized (topSyncAcc) {
+                if (isEnoughMoney(fromAccount.getMoney(), amount)) {
+                    System.out.println("Средств на счету достаточно для перевода.");
+                    if (amount > MAX_AMOUNT) {
+                        System.out.println("Сумма перевода боольше " + MAX_AMOUNT + "."
+                                + "\n\tПодождите несколько секунд идет проверка счета.");
+                        try {
+                            chek = isFraud(fromAccountNum, toAccountNum, amount);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace(System.out);
+                        }
+                        if (chek) {
+                            fromAccount.blockedAccount();
+                            toAccount.blockedAccount();
+                            System.out.println("Перевод не выполнен. Счета " + fromAccount.getAccNumber() + " и " + toAccount.getAccNumber() + " забокированы");
+                        } else {
+                            toAccount.setMoney(toAccount.getMoney() + amount);
+                            fromAccount.setMoney(fromAccount.getMoney() - amount);
+                            System.out.println("Со счета " + fromAccount.getAccNumber() + " было списано " + amount + " и зачислено на счет " + toAccount.getAccNumber());
+                        }
+                    } else {
+                        toAccount.setMoney(toAccount.getMoney() + amount);
+                        fromAccount.setMoney(fromAccount.getMoney() - amount);
+                        System.out.println("Со счета " + fromAccount.getAccNumber() + " было списано " + amount + " и зачислено на счет " + toAccount.getAccNumber());
+                    }
                 }
-                if(chek) {
-                    fromAccount.blockedAccount();
-                    toAccount.blockedAccount();
-                    System.out.println("Перевод не выполнен. Счета " + fromAccount.getAccNumber() + " и " + toAccount.getAccNumber() + " забокированы");
-                }
-                else {
-                    toAccount.setMoney(toAccount.getMoney() + amount);
-                    fromAccount.setMoney(fromAccount.getMoney() - amount);
-                }
-            }
-            else {
-                toAccount.setMoney(toAccount.getMoney() + amount);
-                fromAccount.setMoney(fromAccount.getMoney() - amount);
+                else System.out.println("Средств на счету недостаточно!");
             }
         }
     }
--- Multithreading/Transactions/src/main/java/Main.java
+++ Multithreading/Transactions/src/main/java/Main.java
@@ -4,15 +4,17 @@ public class Main {
         Bank bank = new Bank();
         Account client1 = new Account("0001", 1_000_000);
         Account client2 = new Account("0002", 10_000_000);
+        Account client3 = new Account("0003", 10_000);
 
         bank.addAccountToBank(client1);
         bank.addAccountToBank(client2);
+        bank.addAccountToBank(client3);
 
         Thread thread1 = new Thread(() -> {
             try {
                 bank.transfer(client1.getAccNumber(), client2.getAccNumber(), 51_000);
                 bank.transfer(client2.getAccNumber(), client1.getAccNumber(), 1_000);
-                System.out.println(Thread.currentThread().getName());
+                System.out.println("Нулевой поток (" + Thread.currentThread().getName() + ")");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -23,7 +25,26 @@ public class Main {
                 Thread.sleep(1000);
                 bank.transfer(client2.getAccNumber(), client1.getAccNumber(), 1_000);
                 bank.transfer(client1.getAccNumber(), client2.getAccNumber(), 1_000);
-                System.out.println(Thread.currentThread().getName());
+                System.out.println("Первый поток (" + Thread.currentThread().getName() + ")");
+            } catch(InterruptedException e) {
+                e.printStackTrace(System.out);
+            }
+        });
+
+        Thread thread3 = new Thread(() -> {
+            try {
+                bank.transfer(client3.getAccNumber(), client2.getAccNumber(), 1_000);
+                bank.transfer(client2.getAccNumber(), client3.getAccNumber(), 1_000);
+                System.out.println("Второй поток (" + Thread.currentThread().getName() + ")");
+            } catch (InterruptedException e) {
+                e.printStackTrace(System.out);
+            }
+        });
+
+        Thread thread4 = new Thread(() -> {
+            try {
+                bank.transfer(client2.getAccNumber(), client3.getAccNumber(), 1_000);
+                bank.transfer(client3.getAccNumber(), client2.getAccNumber(), 1_000);
             } catch(InterruptedException e) {
                 e.printStackTrace(System.out);
             }
@@ -31,9 +52,13 @@ public class Main {
 
         thread1.start();
         thread2.start();
+        thread3.start();
+        thread4.start();
 
         thread1.join();
         thread2.join();
+        thread3.join();
+        thread4.join();
 
     }
 }
